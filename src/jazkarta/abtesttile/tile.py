034b5f63265a07ce34b2cf1d3d907546587b3e0b
Alec Mitchell <alecpm@gmail.com>
2018-05-07 20:46:39 -0400

    Make tile persistent. Whitespace fixes. Fix local buildout.

--- src/jazkarta/abtesttile/tile.py
+++ src/jazkarta/abtesttile/tile.py
@@ -1,39 +1,37 @@
 from plone.app.textfield import RichText
-from plone.tiles import Tile
+from plone.tiles import PersistentTile
 from plone.supermodel import model
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from zope import schema
 from . import _
-import random
 
 
 class IABTestTile(model.Schema):
 
     text_a = RichText(
-		title=_(u'Text A'),
-		required=False,
-		allowed_mime_types=('text/html',),
-		default_mime_type='text/html',
-		output_mime_type='text/x-html-safe',
-		default=u'')
+        title=_(u'Text A'),
+        required=False,
+        allowed_mime_types=('text/html',),
+        default_mime_type='text/html',
+        output_mime_type='text/x-html-safe',
+        default=u'')
 
     text_b = RichText(
-		title=_(u'Text B'),
-		required=False,
-		allowed_mime_types=('text/html',),
-		default_mime_type='text/html',
-		output_mime_type='text/x-html-safe',
-		default=u'')
+        title=_(u'Text B'),
+        required=False,
+        allowed_mime_types=('text/html',),
+        default_mime_type='text/html',
+        output_mime_type='text/x-html-safe',
+        default=u'')
 
     ratio = schema.Int(
-		title=_(u'Percentage of time that tile A will be shown'),
-		min=0,
-		max=100,
-		default=50,
+        title=_(u'Percentage of time that tile A will be shown'),
+        min=0,
+        max=100,
+        default=50,
         required=True)
 
 
-class ABTestTile(Tile):
+class ABTestTile(PersistentTile):
     """Existing content tile
     """
 
