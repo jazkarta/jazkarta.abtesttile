eb853bfe5190e5149b9dd9cf07a9797b90150512
Jesse Snyder <jsnyder@wesleyan.edu>
2019-05-08 17:06:58 -0700

    Preparing release 1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='jazkarta.abtesttile',
-    version='1.0',
+    version='1.1',
     description="Plone Mosaic Tile for A/B Testing content changes",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
